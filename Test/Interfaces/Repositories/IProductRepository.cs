57d750398ca8625f3a6e869a43b03724a2ae1c86
kevin-tsao <kevinwei326@gmail.com>
2024-06-24 14:17:42 +0800

    加入Interface

---
+++ Test/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Test.Models;
+
+namespace Test.Interfaces.Repositoies
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Get();
+        Product Get(int id);
+        IHttpActionResult Post(Product product);
+        IHttpActionResult Put(int id, Product product);
+        IHttpActionResult Delete(int id);
+    }
+}
---
+++ Test/Interfaces/Services/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Test.Models;
+
+namespace Test.Interfaces.Services
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> Get();
+        Product Get(int id);
+        IHttpActionResult Post([FromBody] Product product);
+        IHttpActionResult Put(int id, [FromBody] Product product);
+        IHttpActionResult Delete(int id);
+    }
+}
